743193e9ff597bae1bbf294e0d515aa64bbe0a71
Matheus Augusto <matheusalgusto_@hotmail.com>
2023-05-10 03:12:46 -0300

    restructure

--- src/train.py
+++ src/train.py
@@ -1,87 +1,98 @@
-from data_loader import ImageDataLoader
-from transformations import get_train_transform, get_test_transform
-from utils import set_gpu_mode, initialize_wandb, get_balanced_dataset_sampler
-from models.model_factory import get_model
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from model import Net
+from utils import valid_epoch, load_checkpoint, save_checkpoint, make_prediction, get_balanced_dataset_sampler, get_train_transform, get_test_transform, train_epoch, create_timestamp_folder, initialize_wandb, set_gpu_mode, measure_execution_time
 
+import sys
+import config
+from dataset import ImageDataLoader
 from sklearn.model_selection import StratifiedKFold
 from torch.utils.data import DataLoader, Subset
-import torch
+import torch.nn.functional as F
+import wandb
 
-class Config:
-    def __init__(self, model_name, k_folds, num_epochs, data_dir, wandb_on, lr) -> None:
-        self.model_name = model_name
-        self.k_folds = k_folds
-        self.num_epochs = num_epochs
-        self.data_dir = data_dir
-        self.wandb_on = wandb_on
-        self.lr = lr
-
-config = Config(
-    model_name='EfficientNet-b0',
-    k_folds=5,
-    num_epochs=2,
-    data_dir='./data/binary/hiper_normal/',
-    wandb_on=False,
-    lr=1e-4
-)
+# This flag allows you to enable the inbuilt cudnn auto-tuner to find the best algorithm to use for your hardware.
+# benchmark mode is good whenever your input sizes for your network do not vary. This way, cudnn will look for the optimal set of algorithms for that particular configuration (which takes some time). This usually leads to faster runtime.
+# But if your input sizes changes at each iteration, then cudnn will benchmark every time a new size appears, possibly leading to worse runtime performances.
+torch.backends.cudnn.benchmark = True
 
 
+def main():
 
-initialize_wandb(config)
+    print(f'Device is {config.DEVICE}')
+
+    initialize_wandb(config)
+
+    artifact_folder = create_timestamp_folder(config.MODEL_NAME)
+    skfold = StratifiedKFold(n_splits=5, shuffle=True)
+    data_loader = ImageDataLoader(config.DATA_DIR)
+    binary_labels = [sample[1] for sample in data_loader.dataset.samples]
+    for fold, (train_ids, test_ids) in enumerate(skfold.split(data_loader.dataset, binary_labels)):
+
+        train_subset = Subset(data_loader.dataset, train_ids)
+        train_subset.transform = get_train_transform()
+        sampler = get_balanced_dataset_sampler(data_loader, train_ids, train_subset)
+        train_loader = DataLoader(train_subset, batch_size=config.BATCH_SIZE, sampler=sampler, num_workers=config.NUM_WORKERS)
+
+        test_subset = Subset(data_loader.dataset, test_ids) 
+        test_subset.transform = get_test_transform()
+        val_loader = DataLoader(test_subset, batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS, shuffle=True )
 
 
-data_loader = ImageDataLoader(config.data_dir)
+        print(f'Fold  {fold +1}')
 
+        loss_fn = nn.CrossEntropyLoss()
+        model = Net(net_version="b0", num_classes=2).to(config.DEVICE)
+        optimizer = optim.Adam(model.parameters(), lr=config.LEARNING_RATE)
+        scaler = torch.cuda.amp.GradScaler()
+        set_gpu_mode(model)
 
-skfold = StratifiedKFold(n_splits=config.k_folds, shuffle=True)
-binary_labels = [sample[1] for sample in data_loader.dataset.samples]
-bag = []
-for fold, (train_ids, test_ids) in enumerate(skfold.split(data_loader.dataset, binary_labels)):
-# K-fold Cross Validation model evaluation
+        if config.LOAD_MODEL:
+            load_checkpoint(torch.load(config.CHECKPOINT_TO_BE_LOADED), model, optimizer)
 
-    print(f'FOLD {fold +1}')
-    print('--------------------------------')
-
-
-    train_subset = Subset(data_loader.dataset, train_ids)
-    train_subset.transform = get_train_transform()
-    sampler = get_balanced_dataset_sampler(data_loader, train_ids, train_subset)
-    train_loader = DataLoader(train_subset, batch_size=50, sampler=sampler)
-
-    test_subset = Subset(data_loader.dataset, test_ids) 
-    test_subset.transform = get_test_transform()
-    test_loader = DataLoader(test_subset, batch_size=50, shuffle=True )
-    
-
-    model = get_model(config.model_name)
-    set_gpu_mode(model)
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
-    loss_function = nn.BCELoss()
-
-    for epoch in range(1, config.num_epochs+1):
-        print(f'Starting epoch {epoch}')
-
+        valid_epoch(val_loader, model, loss_fn, config.DEVICE)
         
-        current_loss = 0.0
-        for i, data in enumerate(train_loader):
-            # Get inputs
-            inputs, targets = data
-            # Zero the gradients
-            optimizer.zero_grad()
-            # Perform forward pass
-            outputs = model(inputs)
-            loss = loss_function(outputs, targets)
+        max_val_accuracy, min_val_loss = 0, sys.maxsize
+        for epoch in range(config.NUM_EPOCHS):
+                   
 
 
-        # for img, labels in train_loader:
-        #     num_retrievers += torch.sum(labels == 0)
-        #     num_elkhounds += torch.sum(labels == 1)
+            train_metrics = train_epoch(train_loader, model, optimizer, loss_fn, scaler, config.DEVICE)
+            test_metrics = valid_epoch(val_loader, model, loss_fn, config.DEVICE)
 
-    
-    # print(num_retrievers.item())
-    # print(num_elkhounds.item())
-    if fold == 0:
-        break
+            train_loss, train_correct = train_metrics
+            test_loss, test_correct, f1_score = test_metrics
+
+            train_loss = train_loss / len(train_loader.sampler)
+            train_acc = train_correct / len(train_loader.sampler) * 100
+            test_loss = test_loss / len(val_loader.sampler)
+            test_acc = test_correct / len(val_loader.sampler) * 100
+            test_f1_score = f1_score / len(val_loader.sampler) * 100
+
+            print(f"Epoch:{epoch + 1}/{config.NUM_EPOCHS} AVG Training Loss:{train_loss:.3f}, AVG Test Loss:{test_loss:.3f}, AVG Training Acc {train_acc:.2f}%, AVG Test Acc {test_acc:.2f}%, F1-Score {test_f1_score:.2f}")
+            
+            if config.WANDB_ON:
+                wandb.log({
+                'train_loss': train_loss,
+                'train_accuracy': train_acc,
+                'test_loss': test_loss,
+                'test_acc': test_acc,
+                'f1_score': test_f1_score
+                })
+
+            if max_val_accuracy < test_acc:
+                max_val_accuracy = test_acc
+                save_checkpoint(model, optimizer, artifact_folder, 'max_acc', fold)
+
+            if min_val_loss > test_loss:
+                min_val_loss = test_loss
+                save_checkpoint(model, optimizer, artifact_folder, 'min_loss', fold)
+            
 
 
-
+if __name__ == "__main__":
+        measure_execution_time(main())
+        
--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,167 @@
+import wandb
 import torch
-from torch.utils.data import WeightedRandomSampler
+import torch.nn.functional as F
+import os
+from PIL import Image
+import pandas as pd
 import numpy as np
+from tqdm import tqdm
+from torchvision import transforms
+from torch.utils.data import WeightedRandomSampler
+import torch
+from tqdm import tqdm
+import torch.nn.functional as F
+import time
+from pathlib import Path
 from torch import nn
 
-def save_checkpoint(state, fold=""):
-    filename=f"{fold}_checkpoint.pth.tar"
-    print("Saving checkpoint...")
+def train_epoch(loader, model, optimizer, loss_fn, scaler, device):
+    model.train()
+    train_loss = 0.0
+    train_correct = 0
+
+    for batch_idx, (data, y) in enumerate(loader):
+        data = data.to(device=device)
+        y = y.to(device=device)
+        targets = F.one_hot(y, num_classes=2)
+
+        # forward
+        with torch.cuda.amp.autocast():
+            output = model(data)
+            # print(targets.float())
+            loss = loss_fn(output, targets.float())
+
+        # backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        train_loss += loss.item() * data.size(0)
+
+        scores = torch.sigmoid(output)
+        predictions = (scores>0.5).float()
+        _, pred = torch.max(predictions, 1)
+        
+        train_correct += (pred == y ).sum()
+
+    return train_loss, train_correct
+
+def valid_epoch(loader, model, loss_fn , device="cuda"):
+    num_correct = 0
+    num_samples = 0
+    valid_loss = 0.0
+    f1_running = 0.0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+  
+            output = model(x)
+            scores = torch.sigmoid(output)
+            predictions = (scores>0.5).float()
+            
+            target = F.one_hot(y, num_classes=2)
+            loss = loss_fn(output, target.float())
+            valid_loss+=loss.item()*x.size(0)
+
+            _, pred = torch.max(predictions, 1)
+
+            num_correct += (pred == y).sum()
+            num_samples += predictions.shape[0]
+
+            f1_score, precision, recall = compute_metrics(y,pred)
+            f1_running +=f1_score
+
+        return valid_loss, num_correct, f1_running
+          
+
+
+    
+
+def save_checkpoint(model, optimizer, create_timestamp_folder, metric_type, fold=""):
+    state = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+    Path(f'./artifacts/{create_timestamp_folder}').mkdir(exist_ok=True)
+    filename=f"./artifacts/{create_timestamp_folder}/{fold}_fold_{metric_type}_checkpoint.pth.tar"
+    # print("Saving checkpoint...")
     torch.save(state, filename)
 
-def load_checkpoint(checkpoint_path, model, optimizer, device):
-    # TODO: add and check device
-    checkpoint = torch.load(checkpoint_path)
 
-    # load variables
+
+def load_checkpoint(checkpoint, model, optimizer):
+    print("=> Loading checkpoint")
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
-    step = checkpoint['step']
 
-    return step
+def make_prediction(model, transform, rootdir, device):
+    files = os.listdir(rootdir)
+    preds = []
+    model.eval()
+
+    files = sorted(files, key=lambda x: float(x.split(".")[0]))
+    for file in tqdm(files):
+        img = Image.open(os.path.join(rootdir, file))
+        img = transform(img).unsqueeze(0).to(device)
+        with torch.no_grad():
+            pred = torch.sigmoid(model(img))
+            preds.append(pred.item())
+
+
+    df = pd.DataFrame({'id': np.arange(1, len(preds)+1), 'label': np.array(preds)})
+    df.to_csv('submission.csv', index=False)
+    model.train()
+    print("Done with predictions")
+
+
+def get_balanced_dataset_sampler(data_loader, train_ids, train_subset):
+    binary_labels = [sample[1] for sample in data_loader.dataset.samples]
+    class_weights = 1 / np.unique(np.array(binary_labels)[train_ids], return_counts=True)[1] 
+    sample_weights = [0] * len(train_ids)
+    for idx, (data, label) in enumerate(train_subset):
+        class_weight = class_weights[label]
+        sample_weights[idx] = class_weight
+    sampler = WeightedRandomSampler(
+        sample_weights, num_samples=len(sample_weights), replacement=True
+    )
+    return sampler
+
+def get_train_transform():
+    return transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.RandomResizedCrop(224),
+        transforms.ColorJitter(
+            brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+def get_test_transform():
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+
+
+def create_timestamp_folder(model_name):
+    """
+    Create a folder name based on the current timestamp.
+    Returns:
+        folder_name (str): The name of the folder, in the format 'YYYY-MM-DD-HH-MM-SS'.
+    """
+    current_time = time.localtime()
+    folder_name = time.strftime('%Y-%m-%d-%H-%M-%S', current_time)
+    return f'{model_name}-{folder_name}'
+
+
+def initialize_wandb(inputs):
+    if inputs.WANDB_ON:
+        wandb.init(name=inputs.name, project=inputs.PROJECT, entity=inputs.ENTITY)
+        wandb.config = inputs.wandb
 
 
 def set_gpu_mode(model):
@@ -29,26 +173,51 @@ def set_gpu_mode(model):
         print('Using a single GPU\n')
 
 
-def initialize_wandb(inputs):
-    if inputs.wandb_on:
-        wandb.init(name=inputs.name, project=inputs.PROJECT, entity=inputs.ENTITY)
-        wandb.config = inputs.wandb
+
+def compute_metrics(targets, predictions):
+    """
+    Compute various classification metrics including F1 score, precision, and recall.
+
+    Args:
+        targets (torch.Tensor): Ground truth labels.
+        predictions (torch.Tensor): Predicted labels.
+
+    Returns:
+        f1_score (float): F1 score.
+        precision (float): Precision.
+        recall (float): Recall.
+    """
+    # Convert tensors to numpy arrays
+    targets = targets.cpu().numpy()
+    predictions = predictions.cpu().numpy()
+
+    # Calculate true positives, false positives, and false negatives
+    true_positives = ((predictions == 1) & (targets == 1)).sum()
+    false_positives = ((predictions == 1) & (targets == 0)).sum()
+    false_negatives = ((predictions == 0) & (targets == 1)).sum()
+
+    # Calculate precision, recall, and F1 score
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+    f1_score = 2 * ((precision * recall) / (precision + recall))
+
+    return f1_score, precision, recall
 
 
-def get_balanced_dataset_sampler(data_loader, train_ids, train_subset):
+def measure_execution_time(func):
+    """
+    Measure the execution time of a function.
 
+    Args:
+        func (callable): The function to measure execution time for.
 
-    binary_labels = [sample[1] for sample in data_loader.dataset.samples]
-    class_weights = 1 / np.unique(np.array(binary_labels)[train_ids], return_counts=True)[1] 
+    Returns:
+        elapsed_time (float): The elapsed time in seconds.
+    """
+    start_time = time.time()
+    func()  # Execute the function
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
-    sample_weights = [0] * len(train_ids)
-
-
-    for idx, (data, label) in enumerate(train_subset):
-        class_weight = class_weights[label]
-        sample_weights[idx] = class_weight
-
-    sampler = WeightedRandomSampler(
-        sample_weights, num_samples=len(sample_weights), replacement=True
-    )
-    return sampler
+    print("Training elapsed Time:", elapsed_time, "seconds")
+    
