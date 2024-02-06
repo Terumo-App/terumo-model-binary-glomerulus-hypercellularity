19ad631b279fd6b3b7a20ac42d062ec9da7c3394
IgorPBorja <igorpradoborja@gmail.com>
2024-02-06 00:09:34 -0300

    [FEAT]: add automatic best model saving for inference pipeline + best models real weights

--- Makefile
+++ Makefile
@@ -12,8 +12,11 @@ test:
 	bash scripts/03_trial_run.sh
 
 inference:
-	python3 -m scripts.perform_inference_all --checkpoint_dir="artifacts" --test_dataset_root="data/test_binary" --device="cuda" --mode="min_loss" > logs/inference_log_min_loss
-	python3 -m scripts.perform_inference_all --checkpoint_dir="artifacts" --test_dataset_root="data/test_binary" --device="cuda" --mode="max_acc" > logs/inference_log_max_acc
+	python3 -m scripts.perform_inference_all --checkpoint_dir="artifacts" --test_dataset_root="data/test_binary" \
+	 --device="cuda" --mode="min_loss" > logs/inference_log_min_loss \
+	 --best_model_dir="best_model" --overwrite_best_model_dir
+	python3 -m scripts.perform_inference_all --checkpoint_dir="artifacts" --test_dataset_root="data/test_binary" \
+	--device="cuda" --mode="max_acc" > logs/inference_log_max_acc
 
 # Delete all compiled Python files
 clean:
--- scripts/perform_inference_all.py
+++ scripts/perform_inference_all.py
@@ -1,7 +1,9 @@
 import argparse
+import datetime
 
 import os
 import re
+import shutil
 
 import numpy as np
 
@@ -26,6 +28,8 @@ def setup_argparse() -> argparse.ArgumentParser:
     parser.add_argument("--verbose", action="store_true")
     parser.add_argument("--device", type=str, default="cpu")
     parser.add_argument("--mode", type=str, default="min_loss")
+    parser.add_argument("--best_model_dir", type=str, default="")
+    parser.add_argument("--overwrite_best_model_dir", action="store_true", default=False)
     return parser
 
 
@@ -50,28 +54,50 @@ def generate_checkpoint_files(class_name: str, search_dir: str, mode: str = "min
                     filtered_checkpoint_files))
 
 
+def save_best_checkpoint(best_model_dir: str,
+                         class_name: str,
+                         checkpoint_files: list[str],
+                         test_metrics: list[Metrics],
+                         watch_metric: str = "fscore"):
+    if len(checkpoint_files) != len(test_metrics):
+        raise RuntimeWarning(f"List of checkpoint files and list of test metrics differ in size: "
+                             f"{len(checkpoint_files)} and {len(test_metrics)}")
+    idx: int = np.argmax([getattr(t, watch_metric) for t in test_metrics])
+    checkpoint_basename: str = os.path.basename(checkpoint_files[idx])
+    checkpoint_basename = "-".join([class_name, str(datetime.datetime.now()), checkpoint_basename])
+    print(f"Best checkpoint for class {class_name} was {checkpoint_files[idx]}. Copying to {best_model_dir}...")
+    shutil.copy(checkpoint_files[idx], os.path.join(best_model_dir, checkpoint_basename))
+
+
 def main():
     parser = setup_argparse()
     args = parser.parse_args()
-    for class_name in settings.data_processing.class_names:
-        print(f" {class_name} binary classifier ".center(80, '-'))
 
+    # clear best_model_dir
+    if args.best_model_dir and args.overwrite_best_model_dir:
+        print(f"Overwriting contents of directory {args.best_model_dir}")
+        if os.path.isdir(args.best_model_dir):
+            shutil.rmtree(args.best_model_dir)
+        os.makedirs(args.best_model_dir, exist_ok=True)
+
+    # inference for each class
+    for class_name in settings.data_processing.class_names:
         config_file = CONFIG_FILES_MAPPING[class_name]
-        print("Using config file: ", config_file)
         checkpoint_files = generate_checkpoint_files(class_name, search_dir=args.checkpoint_dir, mode=args.mode)
         test_losses: list[float] = []
         test_metrics: list[Metrics] = []
 
+        print(f" {class_name} binary classifier ".center(80, '-'))
+        print("Using config file: ", config_file)
+
         for checkpoint in checkpoint_files:
             if args.verbose:
-                print(f"Fold from checkpoint file '{checkpoint}'", end=2*'\n')
+                print(f"\nFold from checkpoint file '{checkpoint}'", end=2*'\n')
             loss, _, metrics_obj = inference(checkpoint_path=checkpoint,
                                              config_file_path=config_file,
                                              test_data_dir=os.path.join(args.test_dataset_root, f"binary_{class_name}"),
                                              verbose=args.verbose,
                                              device=args.device)
-            if args.verbose:
-                print()
             test_losses.append(loss)
             test_metrics.append(metrics_obj)
 
@@ -84,6 +110,13 @@ def main():
             std = np.std([getattr(t, m_name) for t in test_metrics])
             print(f"test/{m_name} (mean): {mean}")
             print(f"test/{m_name} (std): {std}")
+
+        if args.best_model_dir:  # != ""
+            save_best_checkpoint(args.best_model_dir,
+                                 class_name,
+                                 checkpoint_files,
+                                 test_metrics,
+                                 watch_metric="fscore")
 
 
 if __name__ == '__main__':
