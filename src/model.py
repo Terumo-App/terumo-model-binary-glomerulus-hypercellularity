a1b3483ac908961079c5f16366dfc9ec7d9cd063
Matheus Augusto <matheusalgusto_@hotmail.com>
2024-06-12 08:26:40 -0300

    new changfes for stain augmentation

--- src/model.py
+++ src/model.py
@@ -3,31 +3,15 @@ from efficientnet_pytorch import EfficientNet
 
 from config import settings
 
-class Net(nn.Module):
-    def __init__(self, net_version, num_classes, freeze: bool = False):
-        super(Net, self).__init__()
-        self.backbone = EfficientNet.from_pretrained('efficientnet-'+net_version)
-        self.backbone._fc = nn.Sequential(
-            nn.Linear(settings.model.fcLayer, 512),  
-            nn.LeakyReLU(), 
-            nn.Dropout(p=0.5), 
-            nn.Linear(512, num_classes),  
-        )
-        if freeze:
-            # freeze backbone layers
-            for name, param in self.backbone.named_parameters():
-                if not name.startswith("_fc"):
-                    param.requires_grad = False
-
-    def forward(self, x):
-        return self.backbone(x)
-    
 # class Net(nn.Module):
 #     def __init__(self, net_version, num_classes, freeze: bool = False):
 #         super(Net, self).__init__()
 #         self.backbone = EfficientNet.from_pretrained('efficientnet-'+net_version)
 #         self.backbone._fc = nn.Sequential(
-#             nn.Linear(settings.model.fcLayer, num_classes),
+#             nn.Linear(settings.model.fcLayer, 512),  
+#             nn.LeakyReLU(), 
+#             nn.Dropout(p=0.5), 
+#             nn.Linear(512, num_classes),  
 #         )
 #         if freeze:
 #             # freeze backbone layers
@@ -37,3 +21,19 @@ class Net(nn.Module):
 
 #     def forward(self, x):
 #         return self.backbone(x)
+    
+class Net(nn.Module):
+    def __init__(self, net_version, num_classes, freeze: bool = False):
+        super(Net, self).__init__()
+        self.backbone = EfficientNet.from_pretrained('efficientnet-'+net_version)
+        self.backbone._fc = nn.Sequential(
+            nn.Linear(settings.model.fcLayer, num_classes),
+        )
+        if freeze:
+            # freeze backbone layers
+            for name, param in self.backbone.named_parameters():
+                if not name.startswith("_fc"):
+                    param.requires_grad = False
+
+    def forward(self, x):
+        return self.backbone(x)
